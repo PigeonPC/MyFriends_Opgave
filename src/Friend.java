389f1ff359b5e6694aeb5423f46d05f506139e8d
SGyldenkvist <silke@gyldenkvist.dk>
2025-03-07 15:43:56 +0100

    .

---
+++ src/Friend.java
@@ -0,0 +1,16 @@
+public class Friend extends Person {
+
+    private String rank;
+
+    public Friend(String rank){
+        this.rank = rank;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+}
