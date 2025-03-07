637d93e94c1c98c70f70a5dfa42b5a9d308b0b55
gitgud <jonathanbirkstrom@gmail.com>
2025-03-07 15:22:27 +0100

    .

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
+    public static void run(Scanner scanner) {
+        int choice = Integer.parseInt(scanner.nextLine());
+        switch(choice) {
+            case 1:
+                showList();
+                break;
+            case 2:
+                enterNewFriend();
+                break;
+            case 3:
+                enterNewFamilyMember();
+                break;
+            case 4:
+                deletePerson();
+                break;
+            case 5:
+                saveList();
+                break;
+            case 6:
+                loadList();
+                break;
+            case 9:
+                quit();
+                break;
+            default:
+                quit();
+                break;
+        }
+    }
 }
