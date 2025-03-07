2b3bb06c6bcf109a57dac143e2ccc02d03af6078
sofuswinther <sfswnthr@gmail.com>
2025-03-07 15:24:58 +0100

    tilføjet menu

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,17 @@ public class Main {
     }
 
     public static void run(Scanner scanner) {
+
+        System.out.println("MENU");
+        System.out.println("Choose option:");
+        System.out.println("1. Show list of persons");
+        System.out.println("2. Enter new friend");
+        System.out.println("3. Enter new family member");
+        System.out.println("4. Delete person");
+        System.out.println("5. Save list");
+        System.out.println("6. Load list");
+        System.out.println("9. Quit");
+
         int choice = Integer.parseInt(scanner.nextLine());
         switch(choice) {
             case 1:
